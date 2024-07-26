5bd7ead369fde0ed38cd88c126bfc6dd836f4972
Hamid Asadi <hamid.19471@gmail.com>
2024-07-26 13:43:10 +0330

    Update: config main colors and set container in center

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,23 @@
 import type { Config } from "tailwindcss";
 
+export const colors: Record<string, string> = {
+  primary: "#2761BA",
+  secondary: "#6699E7",
+  white: "#FFFFFF",
+  black: "#000000",
+  transparent: "transparent",
+  gray: "#7E7E7E",
+  success: "#5CB85C",
+  warning: "#F0AD4E",
+  error: "#D9534F",
+  info: "#5BC0DE",
+  "base-500": "#7E7E7E",
+  "base-600": "#5E5E5E",
+  "base-700": "#3D3D3D",
+  "base-800": "#292929",
+  "base-900": "#141414",
+};
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +25,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
+    colors: colors,
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
