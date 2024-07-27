6eb5c1732cea9cebd231b0cad143547fd8a8c570
Hamid Asadi <hamid.19471@gmail.com>
2024-07-28 00:45:35 +0330

    Update: add home service section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./_components/header/hero-section/hero-section";
+import { ServicesSection } from "./_components/home-sections";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <ServicesSection />
     </div>
   );
 }
