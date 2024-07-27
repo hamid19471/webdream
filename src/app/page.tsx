0555efd4affad2fe9e93a8b92572cf5f423c7acc
Hamid Asadi <hamid.19471@gmail.com>
2024-07-27 16:30:39 +0330

    Update: import hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { HeroSection } from "./_components/header/hero-section/hero-section";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 }
