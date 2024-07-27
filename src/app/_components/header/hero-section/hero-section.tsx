c3cf57212746ab52df397aecd67cc22d79edf1b2
Hamid Asadi <hamid.19471@gmail.com>
2024-07-28 00:44:43 +0330

    Update: fix style issue

--- src/app/_components/header/hero-section/hero-section.tsx
+++ src/app/_components/header/hero-section/hero-section.tsx
@@ -5,10 +5,10 @@ import Particles from "../../particles/particles";
 export const HeroSection: React.FC = () => {
   return (
     <div className="w-full">
-      <div className="text-left bg-home-image h-screen -translate-y-24 bg-no-repeat bg-cover lg:bg-contain bg-center lg:bg-right">
-        <div className="lg:container h-full flex flex-col justify-center lg:items-start items-center bg-black/60 xl:bg-black/30">
+      <div className="text-left bg-home-image h-screen -translate-y-24 bg-no-repeat bg-cover lg:bg-contain bg-center lg:bg-right ">
+        <div className="lg:container h-full flex flex-col justify-center lg:items-start items-center bg-black/40  xl:bg-black/20 ">
           <Particles className="absolute inset-0 opacity-55" />
-          <div className="relative flex flex-col gap-4 text-center lg:text-left">
+          <div className="relative flex flex-col gap-4 text-center lg:text-left ">
             <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-white">
               Webdream Studio
             </h1>
